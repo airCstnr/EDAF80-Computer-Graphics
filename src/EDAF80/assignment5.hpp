6bf2139ad7c58836b20caee6b94eb99066d5559d
airCstnr <raphael.castanier@free.fr>
2019-10-19 18:02:27 +0200

    Refactor : moved meshes, uniforms and nodes to functions

--- src/EDAF80/assignment5.hpp
+++ src/EDAF80/assignment5.hpp
@@ -3,8 +3,12 @@
 #include "core/InputHandler.h"
 #include "core/FPSCamera.h"
 #include "core/WindowManager.hpp"
-
 #include "core/ShaderProgramManager.hpp"
+
+#include "core/node.hpp"
+#include "core/helpers.hpp"
+
+#include "external/glad/glad.h"
 
 class Window;
 
@@ -28,6 +32,10 @@ namespace edaf80
 
 		void setup_camera();
 		void setup_program_manager();
+		void setup_meshes();
+		void setup_uniforms();
+		void setup_nodes();
+
 
 		glm::vec3 get_step( float path_pos,
 							std::vector<glm::vec3>& path );
@@ -42,6 +50,7 @@ namespace edaf80
 		WindowManager& _windowManager;
 		GLFWwindow*    _window;
 		ShaderProgramManager _program_manager;
+		float _time = 0.0f;
 
 		// Shaders identifiers
 		GLuint _texcoord_shader;
@@ -50,5 +59,25 @@ namespace edaf80
 		GLuint _water_shader;
 		GLuint _skybox_shader;
 		GLuint _phong_shader;
+
+		// Objects meshes
+		bonobo::mesh_data _water_shape;
+		bonobo::mesh_data _sky_shape;
+		bonobo::mesh_data _dory_shape;
+		bonobo::mesh_data _mine_shape;
+		bonobo::mesh_data _nemo_shape;
+
+
+		std::function<void( GLuint )> _simple_set_uniforms;
+		std::function<void( GLuint )> _phong_set_uniforms;
+		std::function<void( GLuint )> _nemo_set_uniforms;
+
+		// Assert nodes
+		Node _water_node;
+		Node _sky_node;
+		Node _dory_node;
+		Node _mine_node;
+		Node _nemo_node;
+
 	};
 }
